a37805cf17262e7f68a2d873f913c9a374631b37
DOT-EXECUTE <dotexecute853@gmail.com>
2026-07-21 19:34:26 +1000

    Remove: Removed Build Dir

--- build/CMakeFiles/cpong.dir/src/globals.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/cpong.dir/src/globals.c.o: \
- /home/aurora/Documents/projects/cpong/src/globals.c \
- /usr/include/stdc-predef.h \
- /home/aurora/Documents/projects/cpong/include/globals.h
